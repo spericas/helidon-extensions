261f70633935b38eee27b7edeb2af8ffa1ab1cff
Tomas Langer <tomas.langer@oracle.com>
2026-03-17 00:46:50 +0100

    Lc4j extension (#7)
    * Add lanchain4j integration extension

---
+++ langchain4j/docs/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+Langchain4j integration extension.
---
+++ langchain4j/modules/providers/oracle-genai/src/main/java/io/helidon/extensions/langchain4j/providers/oci/genai/OciFactoryMethods.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.extensions.langchain4j.providers.oci.genai;
+
+import io.helidon.builder.api.Prototype;
+import io.helidon.config.Config;
+
+import com.oracle.bmc.Region;
+import com.oracle.bmc.generativeaiinference.model.ServingMode;
+
+/**
+ * Config mappers for special OCI SDK types.
+ */
+final class OciFactoryMethods {
+
+    private OciFactoryMethods() {
+    }
+
+    @Prototype.ConfigFactoryMethod("region")
+    static Region createRegion(Config config) {
+        return config.asString()
+                .map(Region::fromRegionCodeOrId)
+                .get();
+    }
+
+    @Prototype.ConfigFactoryMethod("servingType")
+    static ServingMode.ServingType createServingType(Config config) {
+        return config.asString()
+                .map(ServingMode.ServingType::create)
+                .get();
+    }
+}
